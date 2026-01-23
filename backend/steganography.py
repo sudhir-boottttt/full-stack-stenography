a92857c1970626dd9be4dfdd7afcf123f0dfa3f6
Sudhir kumar <65527808+sudhir-boottttt@users.noreply.github.com>
2026-01-23 14:48:42 +0530

    change code cmpletly

--- backend/steganography.py
+++ backend/steganography.py
@@ -2,67 +2,75 @@ from PIL import Image
 
 STOP_MARKER = "###END###"
 
-def text_to_binary(text):
-    return [format(ord(c), '08b') for c in text]
+def gen_data(data):
+    return [format(ord(i), '08b') for i in data]
 
+def modify_pixels(pixels, data):
+    datalist = gen_data(data)
+    data_len = len(datalist)
+    pixel_iter = iter(pixels)
 
-def modify_pixels(pixels, binary_data):
-    data_index = 0
-    new_pixels = []
+    for i in range(data_len):
+        pixel_block = [
+            value
+            for value in next(pixel_iter)[:3]
+            + next(pixel_iter)[:3]
+            + next(pixel_iter)[:3]
+        ]
 
-    for pixel in pixels:
-        if data_index >= len(binary_data):
-            new_pixels.append(pixel)
-            continue
+        for j in range(8):
+            if datalist[i][j] == '0' and pixel_block[j] % 2 != 0:
+                pixel_block[j] -= 1
+            elif datalist[i][j] == '1' and pixel_block[j] % 2 == 0:
+                pixel_block[j] += 1
 
-        r, g, b = pixel
-        bits = binary_data[data_index]
+        # STOP FLAG
+        if i == data_len - 1:
+            if pixel_block[-1] % 2 == 0:
+                pixel_block[-1] += 1
+        else:
+            if pixel_block[-1] % 2 != 0:
+                pixel_block[-1] -= 1
 
-        r = r - 1 if bits[0] == '0' and r % 2 != 0 else r
-        r = r + 1 if bits[0] == '1' and r % 2 == 0 else r
-
-        g = g - 1 if bits[1] == '0' and g % 2 != 0 else g
-        g = g + 1 if bits[1] == '1' and g % 2 == 0 else g
-
-        b = b - 1 if bits[2] == '0' and b % 2 != 0 else b
-        b = b + 1 if bits[2] == '1' and b % 2 == 0 else b
-
-        new_pixels.append((r, g, b))
-        data_index += 1
-
-    return new_pixels
-
+        yield tuple(pixel_block[:3])
+        yield tuple(pixel_block[3:6])
+        yield tuple(pixel_block[6:9])
 
 def encode_image(image, message):
     message += STOP_MARKER
-    binary_data = text_to_binary(message)
+    new_image = image.copy()
+    width = new_image.size[0]
 
-    pixels = list(image.getdata())
-    new_pixels = modify_pixels(pixels, binary_data)
+    x = y = 0
+    for pixel in modify_pixels(new_image.getdata(), message):
+        new_image.putpixel((x, y), pixel)
+        if x == width - 1:
+            x = 0
+            y += 1
+        else:
+            x += 1
 
-    encoded_image = image.copy()
-    encoded_image.putdata(new_pixels)
-    return encoded_image
-
+    return new_image
 
 def decode_image(image):
-    pixels = list(image.getdata())
-    binary = ""
+    data = ""
+    pixel_iter = iter(image.getdata())
 
-    for r, g, b in pixels:
-        binary += str(r % 2)
-        binary += str(g % 2)
-        binary += str(b % 2)
+    while True:
+        pixels = [
+            value
+            for value in next(pixel_iter)[:3]
+            + next(pixel_iter)[:3]
+            + next(pixel_iter)[:3]
+        ]
 
-    chars = [binary[i:i+8] for i in range(0, len(binary), 8)]
-    message = ""
+        binary = ""
+        for i in pixels[:8]:
+            binary += '0' if i % 2 == 0 else '1'
 
-    for c in chars:
-        try:
-            message += chr(int(c, 2))
-            if message.endswith(STOP_MARKER):
-                return message.replace(STOP_MARKER, "")
-        except:
+        data += chr(int(binary, 2))
+
+        if pixels[-1] % 2 != 0:
             break
 
-    return message
+    return data.replace(STOP_MARKER, "")
